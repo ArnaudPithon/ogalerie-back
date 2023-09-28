ab11f9691d9f807fbe0897c85a92f9a859d6a7af
Arnaud <arnaud@icarios.net>
2023-09-28 03:05:19 +0200

    FIX: suppression d'un appel à un contrôleur obsolète

--- src/api/controllers/index.js
+++ src/api/controllers/index.js
@@ -1,9 +1,8 @@
 'use strict';
 
-const authController = require('./authenticate.js');
 const usersController = require('./users.js');
 //const artworksController = require('./artworks.js');
 //const collectionsController = require('./collections.js');
 //const tagsController = require('./tags.js');
 
-module.exports = { authController, usersController };
+module.exports = { usersController };
