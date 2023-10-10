1f9615f73d50d5a9ee24463120bb676b1a02f2d6
Arnaud <arnaud@icarios.net>
2023-10-10 23:20:26 +0200

    Définition des informations de l'API

--- src/api/routers/index.js
+++ src/api/routers/index.js
@@ -13,7 +13,26 @@ const swaggerDefinition = {
     info: {
         title: 'Express API for Ogalerie',
         version: '1.0.0',
+        description: 'This is a REST API application made with Express.',
+        license: {
+            name: 'Licensed under MIT',
+            url: 'https://mit-license.org/',
+        },
+        contact: {
+            name: 'Ogalerie',
+            url: 'https://github.com/O-clock-Yost/projet-06-o-galerie-back',
+        },
     },
+    servers: [
+        {
+            url: 'http://localhost:3003',
+            description: 'Development server',
+        },
+        {
+            url: 'http://arnaudpithon-server.eddi.cloud:8080',
+            description: 'Production Server OClock',
+        },
+    ],
 };
 const options = {
     swaggerDefinition,
