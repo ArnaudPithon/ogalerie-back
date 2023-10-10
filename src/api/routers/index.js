2efb995f7141907ab17ba1d250689649fe75beba
Arnaud <arnaud@icarios.net>
2023-10-10 23:07:55 +0200

    Mise en place initiale d'une doc API
    JSDoc - Swagger - OpenAPI

--- src/api/routers/index.js
+++ src/api/routers/index.js
@@ -3,7 +3,26 @@
 const router = require('express').Router();
 const errorHandler = require('../services/errorHandler');
 
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
 const routerAPI = require('./router.js');
+
+const swaggerDefinition = {
+    openapi: '3.1.0',
+    info: {
+        title: 'Express API for Ogalerie',
+        version: '1.0.0',
+    },
+};
+const options = {
+    swaggerDefinition,
+    // Paths to files containing OpenAPI definitions
+    apis: ['./api/routers/*.js'],
+};
+const swaggerSpec = swaggerJSDoc(options);
+
+router.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Les urls préfixées par /v1 rentrent dans routerAPI
 router.use('/v1', routerAPI);
