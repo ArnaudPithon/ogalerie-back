0c9484026e445457813234722bf8c0c96d0d711e
icarios <arnaud@icarios.net>
2025-08-02 00:05:10 +0200

    style(server): indentation

--- src/server.js
+++ src/server.js
@@ -22,29 +22,29 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: true,
-    saveUninitialized: false,
-    cookie: {
-        // options pour le cookie
-    },
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: false,
+  cookie: {
+    // options pour le cookie
+  },
 }));
 
 app.use(routers);
 
 app.listen(PORT, () => {
-    console.log(`listening at http://localhost:${PORT} …`);
+  console.log(`listening at http://localhost:${PORT} …`);
 });
 
 try {
-    const server = https.createServer({
-        key: fs.readFileSync(`${__dirname}/../ssl/privkey.pem`),
-        cert: fs.readFileSync(`${__dirname}/../ssl/fullchain.pem`),
-    }, app);
+  const server = https.createServer({
+    key: fs.readFileSync(`${__dirname}/../ssl/privkey.pem`),
+    cert: fs.readFileSync(`${__dirname}/../ssl/fullchain.pem`),
+  }, app);
 
-    (async () => {
-        await server.listen(PORTS);
-    })();
+  (async () => {
+    await server.listen(PORTS);
+  })();
 } catch (err) {
-    console.error(err);
+  console.error(err);
 }
