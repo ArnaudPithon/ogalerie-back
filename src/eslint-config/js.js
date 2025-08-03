a0f3df049b48b568b27ef95f44b90c3fdc4c1a1b
icarios <arnaud@icarios.net>
2025-08-03 21:10:51 +0200

    chore(lint): modularisation plus poussée de la conf eslint

--- src/eslint-config/js.js
+++ src/eslint-config/js.js
@@ -3,6 +3,8 @@ import eslintPluginImport from 'eslint-plugin-import';
 import eslintPluginNode from 'eslint-plugin-node';
 import eslintPluginPromise from 'eslint-plugin-promise';
 
+import { commonRules } from './common-rules.js';
+
 export default {
   files: ['**/*.{js,mjs,cjs}'],
   languageOptions: {
@@ -15,18 +17,9 @@ export default {
     promise: eslintPluginPromise,
   },
   rules: {
+    ...commonRules,
     'no-console': 'off',
     'import/order': ['warn', { 'newlines-between': 'always' }],
     'node/no-unsupported-features/es-syntax': 'off',
-    camelcase: ['error'],
-    indent: ['error', 2],
-    quotes: ['error', 'single', { avoidEscape: true }],
-    semi: ['error', 'always'],
-    'arrow-spacing': ['error'],
-    'block-spacing': ['error', 'always'],
-    'comma-dangle': ['error', 'always-multiline'],
-    'comma-spacing': ['error', { before: false, after: true }],
-    'comma-style': ['error', 'last'],
-    'dot-notation': ['error', { allowPattern: '^[a-z]+(_[a-z]+)+$' }],
   },
 };
--- src/eslint-config/ts.js
+++ src/eslint-config/ts.js
@@ -1,3 +1,5 @@
+import { commonRules } from './common-rules';
+
 export default {
   files: ['**/*.ts'],
   languageOptions: {
@@ -13,9 +15,31 @@ export default {
     '@typescript-eslint': await import('@typescript-eslint/eslint-plugin'),
   },
   rules: {
+    ...commonRules,
     '@typescript-eslint/no-unused-vars': ['warn'],
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/ban-ts-comment': 'warn',
+
+    // Type-aware (nécessite parserOptions.project)
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/no-misused-promises': 'error',
+    '@typescript-eslint/no-unnecessary-type-assertion': 'warn',
+    '@typescript-eslint/no-unnecessary-type-constraint': 'warn',
+    '@typescript-eslint/restrict-template-expressions': [
+      'warn',
+      {
+        allowNumber: true,
+        allowBoolean: false,
+        allowAny: false,
+        allowNullish: false,
+      },
+    ],
+    '@typescript-eslint/prefer-includes': 'warn',
+    '@typescript-eslint/prefer-nullish-coalescing': 'warn',
+    '@typescript-eslint/prefer-optional-chain': 'warn',
+    '@typescript-eslint/await-thenable': 'warn',
+    '@typescript-eslint/consistent-type-imports': 'warn',
+    '@typescript-eslint/no-confusing-void-expression': ['warn', { ignoreArrowShorthand: true }],
   },
 };
