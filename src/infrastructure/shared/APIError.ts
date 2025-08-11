6916fcf10ee67bed95b481fde83bfcacfca3f86e
icarios <arnaud@icarios.net>
2025-08-11 12:38:02 +0200

    refactor(typescript): convert infrastructure/shared/APIError

---
+++ src/infrastructure/shared/APIError.ts
@@ -0,0 +1,19 @@
+class APIError extends Error {
+  error?: Error;
+  code: number;
+  date: Date;
+
+  constructor(message: string, code: number, err: Error) {
+    // Parent constructor
+    super(message);
+
+    if (err) {
+      this.error = err;
+    }
+
+    this.code = code;
+    this.date = new Date();
+  }
+}
+
+export default APIError;
