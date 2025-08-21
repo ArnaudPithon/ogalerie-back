363a3cf6ecff69f334acdde919c73e81496384b9
icarios <arnaud@icarios.net>
2025-08-21 12:15:51 +0200

    refactor(typescript): convert tags/model

---
+++ src/modules/tags/types.ts
@@ -0,0 +1,12 @@
+export type Tag = {
+  id: number;
+  title: string;
+  uri: string;
+  date: Date;
+  description: string;
+  mature: boolean;
+  collectionId: number;
+  ownerId: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
