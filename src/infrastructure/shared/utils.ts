ff2a264b24092e39713eb4f4084e81550ed7c04f
icarios <arnaud@icarios.net>
2025-08-23 11:14:56 +0200

    feat(shared/utils): Implement a robust assertion function.
    Exported `assert` function checks the provided value and throws an error
    if it's falsy, with an optional error message.
    
    The function uses TypeScript's built-in `asserts` type to ensure type
    safety. This change enhances the utility module by providing a
    standardized way to verify values in critical sections of code.

--- src/infrastructure/shared/utils.ts
+++ src/infrastructure/shared/utils.ts
@@ -1,1 +1,12 @@
-export const toBool = (x: unknown): boolean => Boolean(x);
+/**
+ * @summary Vérification d'une assertion
+  * @param value Valeur à vérifier
+  * @param message Message d'erreur si l'assertion échoue
+  * @throws Error si l'assertion échoue
+  * @returns void si l'assertion réussit
+  */
+export function assert(value: unknown, message?: string): asserts value {
+  if (!value) {
+    throw new Error(message ?? 'value is falsy');
+  }
+}
