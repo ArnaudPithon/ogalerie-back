553fc5be8531bbec7ec917ff3ad9c0f6d5a5e09d
icarios <arnaud@icarios.net>
2025-08-26 22:19:15 +0200

    refactor(tsoa): intégration de tsoa dans le serveur Express

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import 'dotenv/config';
 import express from 'express';
 import session from 'express-session';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import { RegisterRoutes } from './routes/routes.ts';
 
 import { httpLogger, logger } from '@/interfaces/logger/logger.js';
 import { errorHandler } from '@/interfaces/http/middlewares/errorHandler.js';
@@ -49,6 +51,15 @@ app.use(
   * @description Routeur principal de l'application
   */
 app.use(router);
+
+// routes auto-générées par tsoa
+RegisterRoutes(app);
+
+/**
+  * @description Route pour la documentation Swagger
+  */
+import swaggerDoc from '../build/swagger.json' with { type: 'json' };
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 /**
   * @description Middleware global de gestion d'erreur
